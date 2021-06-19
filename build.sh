288fbea2749d6db1d9253314ce19b0fb80c6f965
misar <fhuanweb@gmail.com>
2021-06-19 16:37:50 +0800

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#cd ..
+cd /
 
 env=${SET_ENV}
 des=${DES}
@@ -14,6 +14,13 @@ version="$(cat pubspec.yaml | shyaml get-value version)"
 # shellcheck disable=SC2002
 flutter_version=$(cat .fvm/fvm_config.json | jq -r '.flutterSdkVersion')
 
+mkdir -p app
+apk_path=app/${env}
+
+if [ -d "$apk_path" ]; then
+  rm -rf $apk_path
+fi
+mkdir -p $apk_path
 
 echo "${des}===android====${env}"
 echo "清理 build"
@@ -21,32 +28,28 @@ echo "清理 build"
 find . -d -name "build" | xargs rm -rf
 
 #fvm use "${flutter_version}" --force
-fvm install "${flutter_version}"
-fvm use "${flutter_version}"
-
+if [ ! -d ".fvm/flutter_sdk" ]; then
+  fvm install "${flutter_version}"
+  fvm use "${flutter_version}"
+fi
 
 fvm flutter clean
-mkdir -p app
-apk_path=app/android-${env}
-
-if [ ! -d "$apk_path" ]; then
-  rm -rf $apk_path
-  mkdir -p $apk_path
-else
-  rm -rf $apk_path
-  mkdir -p $apk_path
-  echo dir exist
-fi
 
 echo "flutter packages get...."
 fvm flutter packages get
 
 echo "build package...."
 fvm flutter build apk -t lib/main_"${env}".dart --no-tree-shake-icons --release --flavor "${env}" #--no-codesign
-echo "build package successed"
+echo "build successful"
 
 cp -r build/app/outputs/apk/"${env}"/release/app-"${env}"-release.apk ${apk_path}
 mv ${apk_path}/app-"${env}"-release.apk "${apk_path}"/v${version}-"${env}".apk
 
-
-
+#cat ./scripts/ver.json | jq ".android"
+echo "{
+  \"android\": \"${version}\",
+  \"android_updateurl\": \"http://101.37.149.8/kkingpro/${env}/android/v${version}-${env}.apk\",
+  \"android_updatetype\": \"noforce\",
+  \"app_name\": \"kking\",
+  \"update_info\": \"${des}\"
+}" >./scripts/ver.json
